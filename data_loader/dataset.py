0d335b8c7c34c3b6d71b55f8d0e25105c8ea22b7
zhangxih21 <zhangxih17@tsinghua.org.cn>
2023-09-17 20:03:38 +0800

    v0.2: exiDLoader

--- data_loader/dataset.py
+++ data_loader/dataset.py
@@ -2,9 +2,12 @@
 from functools import lru_cache
 from pathlib import Path
 from typing import Any, List, Sequence, Union
-from data_loader.data_process import *
+from data_process import *
 import torch
+import torch.utils.data as torch_data
 from torch.utils.data import DataLoader,Dataset
+import dgl
+from exiD_loader import *
 
 max_nodes = 20
 max_lanes = 8
@@ -12,236 +15,18 @@ max_frames = 50
 max_objects = 10
 num_agents = 4
 
-@lru_cache(128)
-def _read_csv(path: Path, *args: Any, **kwargs: Any) -> pd.DataFrame:
-    """A caching CSV reader
-
-    Args:
-        path: Path to the csv file
-        *args, **kwargs: optional arguments to be used while data loading
-
-    Returns:
-        pandas DataFrame containing the loaded csv
-    """
-    return pd.read_csv(path, *args, **kwargs)
-
-
-class ArgoverseForecastingLoader:
-    def __init__(self, root_dir: Union[str, Path]):
-        """Initialization function for the class.
-
-        Args:
-            root_dir: Path to the folder having sequence csv files
-        """
-        self.counter: int = 0
-
-        root_dir = Path(root_dir)
-        self.seq_list: Sequence[Path] = [(root_dir / x).absolute() for x in os.listdir(root_dir)]
-
-        self.current_seq: Path = self.seq_list[self.counter]
-
-    @property
-    def track_id_list(self) -> List[int]:
-        """Get the track ids in the current sequence.
-
-        Returns:
-            list of track ids in the current sequence
-        """
-        _track_id_list: List[int] = np.unique(self.seq_df["trackId"].values).tolist()
-        return _track_id_list
-
-    @property
-    def num_tracks(self) -> int:
-        """Get the number of tracks in the current sequence.
-
-        Returns:
-            number of tracks in the current sequence
-        """
-        return len(self.track_id_list)
-
-    @property
-    def seq_df(self) -> pd.DataFrame:
-        """Get the dataframe for the current sequence.
-
-        Returns:
-            pandas DataFrame for the current sequence
-        """
-        return _read_csv(self.current_seq)
-
-    @property
-    def agent_traj(self) -> np.ndarray:
-        """Get the trajectory for the track of type 'AGENT' in the current sequence.
-
-        Returns:
-            numpy array of shape (seq_len x 2) for the agent trajectory
-        """
-        #test
-        # print(self.seq_df.columns.values)
-        agent_id = self.seq_df[self.seq_df["class"] == "agent"]["trackId"].unique()
-        agent_x, agent_y = [], []
-        for id in list(agent_id):
-          agent_x.append(self.seq_df[(self.seq_df["class"] == "agent") \
-                        & (self.seq_df["trackId"] == int(id))]["xCenter"])
-          agent_y.append(self.seq_df[(self.seq_df["class"] == "agent") \
-                        & (self.seq_df["trackId"] == int(id))]["yCenter"])
-        # print('agent_x: ', len(agent_x), len(agent_x[0]))
-        agent_traj = np.column_stack((agent_x, agent_y))
-        # print('agent_traj: ', agent_traj.shape)
-        return agent_traj # [4,50,50]
-
-    @property
-    def agent_info(self) -> np.ndarray:
-        """Get the trajectory for the track of type 'AGENT' in the current sequence.
-
-        Returns:
-            numpy array of shape (seq_len x 2) for the agent trajectory
-        """
-        agent_id = self.seq_df[self.seq_df["class"] == "agent"]["trackId"].unique()
-        agent_x, agent_y = [], []
-        vel_x, vel_y = [], []
-        heading = []
-        for id in list(agent_id):
-          temp_df = self.seq_df[(self.seq_df["class"] == "agent") \
-                        & (self.seq_df["trackId"] == int(id))]
-          # print('agent columns ', temp_df.columns.values)
-          agent_x.append(temp_df["xCenter"])
-          agent_y.append(temp_df["yCenter"])
-          vel_x.append(temp_df["xVelocity"])
-          vel_y.append(temp_df["yVelocity"])
-          heading.append(temp_df["heading"])
-
-        agent_info = np.column_stack((agent_x, agent_y, vel_x, vel_y, heading))
-        # print('agent_info: ', agent_info.shape, agent_info[0,:])
-        return agent_info # [4,50,5] agents, frames, features
-
-    @property
-    def obj_info(self) -> np.ndarray:
-        """Get the trajectory for the track of type 'OTHER' in the current sequence.
-
-        Returns:
-            numpy array of shape (seq_len x 2) for the agent trajectory
-        """
-        obj_id = self.seq_df[self.seq_df["class"] != "agent"]["trackId"].unique()
-        agent_x, agent_y = [], []
-        vel_x, vel_y = [], []
-        heading = []
-        start_frame = min(self.seq_df["frame"])
-        end_frame = max(self.seq_df["frame"])
-        # print('start - end ', start_frame, end_frame)
-        for id in list(obj_id):
-          temp_df = self.seq_df[(self.seq_df["class"] != "agent") \
-                        & (self.seq_df["trackId"] == int(id))].reset_index()
-          # print(temp_df["xCenter"][0:5])
-          # 序列缺失值填补
-          df_start = max(0, min(temp_df["frame"]) - start_frame)
-          df_end = min(max(temp_df["frame"]) - start_frame + 1, max_frames)
-          # print('df ', df_start, df_end, len(temp_df["xCenter"]))
-          temp_x, temp_y, temp_vx, temp_vy, temp_h = [0]*max_frames,[0]*max_frames,[0]*max_frames,[0]*max_frames,[0]*max_frames
-          # print( temp_df["xCenter"][0])
-          for i in range(df_start, df_end):
-            temp_x[i] = temp_df["xCenter"][i-df_start]
-            temp_y[i] = temp_df["yCenter"][i-df_start]
-            temp_vx[i] = temp_df["xVelocity"][i-df_start]
-            temp_vy[i] = temp_df["yVelocity"][i-df_start]
-            temp_h[i] = temp_df["heading"][i-df_start]
-          # print('temp x ', temp_x)
-          agent_x.append(temp_x)
-          agent_y.append(temp_y)
-          vel_x.append(temp_vx)
-          vel_y.append(temp_vy)
-          heading.append(temp_h)
-
-        obj_info = np.column_stack((agent_x, agent_y, vel_x, vel_y, heading))
-        # print('obj_info ', obj_info.shape)
-        return obj_info # [4,50,5] objs, frames, features
-
-    def __iter__(self) -> "ArgoverseForecastingLoader":
-        """Iterator for enumerating over sequences in the root_dir specified.
-
-        Returns:
-            Data Loader object for the first sequence in the data
-        """
-        self.counter = 0
-        return self
-
-    def __next__(self) -> "ArgoverseForecastingLoader":
-        """Get the Data Loader object for the next sequence in the data.
-
-        Returns:
-            Data Loader object for the next sequence in the data
-        """
-        if self.counter >= len(self):
-            raise StopIteration
-        else:
-            self.current_seq = self.seq_list[self.counter]
-            self.counter += 1
-            return self
-
-    def __len__(self) -> int:
-        """Get the number of sequences in the data
-
-        Returns:
-            Number of sequences in the data
-        """
-        return len(self.seq_list)
-
-    def __str__(self) -> str:
-        """Decorator that returns a string storing some stats of the current sequence
-
-        Returns:
-            A string storing some stats of the current sequence
-        """
-        return f"""Seq : {self.current_seq}
-        ----------------------
-        || City: {self.city}
-        || # Tracks: {len(self.track_id_list)}
-        ----------------------"""
-
-    def __getitem__(self, key: int) -> "ArgoverseForecastingLoader":
-        """Get the DataLoader object for the sequence corresponding to the given index.
-
-        Args:
-            key: index of the element
-
-        Returns:
-            Data Loader object for the given index
-        """
-
-        self.counter = key
-        self.current_seq = self.seq_list[self.counter]
-        return self
-
-    def get(self, seq_id: Union[Path, str]) -> "ArgoverseForecastingLoader":
-        """Get the DataLoader object for the given sequence path.
-
-        Args:
-            seq_id: Fully qualified path to the sequence
-
-        Returns:
-            Data Loader object for the given sequence path
-        """
-        self.current_seq = Path(seq_id).absolute()
-        return self
-
 
 def get_lane_centerlines(argoverse_data, lane_centerlines_dict, agent_lane_dict, left_id_dict, right_id_dict):
   '''
   根据车辆位置信息和地图，获取周围的车道信息
   Args:
-    argoverse_data: afl, 获取车辆轨迹, 包含车道id, traj_xy
+    argoverse_data: edl, 获取车辆轨迹, 包含车道id, traj_xy
   return:
     lane_centerlines: list [[]]
   '''
   agent_obs_traj = argoverse_data.agent_traj
   track_id = int(argoverse_data.seq_df[argoverse_data.seq_df["class"] == "agent"]["trackId"].unique()[0])
   lane_centerlines_id = agent_lane_dict[track_id]
-  # for id in data['laneletId']:
-  #   if len(id) < 6:
-  #     lane_centerlines_id.add(id)
-  #   else:
-  #     ids = id.split(';')
-  #     lane_centerlines_id.add(id[0])
-  #     lane_centerlines_id.add(id[1])
 
   lane_centerlines = []
   for id in lane_centerlines_id:
@@ -296,18 +81,73 @@ def compose_graph(lane,label):
     return graph,features
 
 
+def collate(samples):
+  datas,labels = map(list,zip(*samples))
+  AgentGraph = [data['Agent'] for data in datas]
+  Center = [data['centerAgent'] for data in datas]
+  Mask = [data['Mapmask'] for data in datas]
+  # batched_graph = dgl.batch(AgentGraph)
+
+  map_set = []
+  feature_set = []
+  for index in range(max_lanes):
+    map_batch = []
+    for data in datas:
+      map_batch.append(data['Map'][index])
+    mgraph = dgl.batch(map_batch)
+    map_set.append(mgraph)
+    feature_set.append(mgraph.ndata['v_feature'])
+
+  agent_set = []
+  agent_feature_set = []
+  for index in range(4): #agents num
+    agent_batch = []
+    for data in datas:
+      agent_batch.append(data['Agent'][index])
+    agraph = dgl.batch(agent_batch)
+    agent_set.append(agraph)
+    agent_feature_set.append(agraph.ndata['v_feature'])
+
+  obj_set = []
+  obj_feature_set = []
+  for index in range(max_objects): #20
+    obj_batch = []
+    for data in datas:
+      obj_batch.append(data['Obj'][index])
+    ograph = dgl.batch(obj_batch)
+    obj_set.append(ograph)
+    obj_feature_set.append(ograph.ndata['v_feature'])
+
+  new_data = {}
+  new_data['Map'] = map_set
+  new_data['Mapfeature'] = torch.stack(feature_set,dim = 0)
+  # new_data['Agent'] = batched_graph
+  # new_data['Agentfeature'] = batched_graph.ndata['v_feature']
+  new_data['Agent'] = agent_set
+  new_data['Agentfeature'] = torch.stack(agent_feature_set,dim = 0)
+  new_data['Obj'] = obj_set
+  new_data['Objfeature'] = torch.stack(obj_feature_set,dim = 0)
+  new_data['centerAgent'] = Center
+  new_data['Mapmask'] = torch.stack(Mask,dim = 0)
+  #new_label = []
+  #for l in labels:
+      #new_label += list(l.flatten())
+  # return new_data,torch.stack(labels,dim = 0).reshape(-1,60)
+  return new_data,torch.stack(labels,dim = 0).reshape(-1,150) #30*5
+
+
 class MTRDataset(torch_data.Dataset):
     '''
     数据集
     '''
-    def __init__(self, root, train = True,test = False):
+    def __init__(self, root, map_root, train = True,test = False):
         '''
         根据路径获得数据，并根据训练、验证、测试划分数据
         train_data 和 test_data路径分开
         '''
         self.test = test
         self.train = train
-        self.afl = ArgoverseForecastingLoader(root)
+        self.edl = exiDLoader(root, map_root)
         # self.avm = ArgoverseMap()
         root_dir = Path(root)
         r = [(root_dir / x).absolute() for x in os.listdir(root_dir)]
@@ -328,14 +168,16 @@ class MTRDataset(torch_data.Dataset):
         从csv创建图输入模型
         '''
         data = {}
-        # agent_data = pd.read_csv(self.root_dir)
-        lane_centerlines = get_lane_centerlines(self.afl[self.start+index], lane_centerlines_dict)
+        agent_lane_dict = self.edl[self.start+index].agent_lane_dict
+        lane_centerlines_dict = self.edl[self.start+index].lane_centerlines_dict
+        left_id_dict, right_id_dict = self.edl[self.start+index].left_right_dict
+        lane_centerlines = get_lane_centerlines(self.edl[self.start+index], lane_centerlines_dict, agent_lane_dict, left_id_dict, right_id_dict)
         # print('lane dim ', len(lane_centerlines[0]), len(lane_centerlines[0][0]))
-        agent_obs_traj = self.afl[index].agent_traj # num_agents: 4
-        center_obs_info = self.afl[index].agent_info #4,250 [x,y,vx,vy,h]
+        agent_obs_traj = self.edl[index].agent_traj # num_agents: 4
+        center_obs_info = self.edl[index].agent_info #4,250 [x,y,vx,vy,h]
         # print(center_obs_info.shape)
         # print('center_obs_info ', center_obs_info[:,0])
-        obj_obs_info = self.afl[index].obj_info
+        obj_obs_info = self.edl[index].obj_info
         # print('obj_obs_info ', obj_obs_info.shape)
         data['centerAgent'] = [center_obs_info[1,19], center_obs_info[1,max_frames+19], \
                       center_obs_info[1,2*max_frames+19], center_obs_info[1,3*max_frames+19], \
@@ -445,6 +287,8 @@ class MTRDataset(torch_data.Dataset):
         return self.end - self.start
 
 
-
-
-
+#test
+train_data_root = 'data/train/0/00'
+map_root = 'data/maps/0_cologne_butzweiler.osm'
+train_data = MTRDataset(train_data_root, map_root, train = True)
+print(train_data[0].agent_lane_dict)
---
+++ data_loader/exiD_loader.py
@@ -0,0 +1,327 @@
+
+import pandas as pd
+import numpy as np
+import utm
+import xml.etree.ElementTree as ET
+import osmnx as ox
+import geopandas as gpd
+
+from functools import lru_cache
+from pathlib import Path
+from typing import Any, List, Sequence, Union
+from data_process import *
+import torch
+import torch.utils.data as torch_data
+from torch.utils.data import DataLoader,Dataset
+
+
+max_nodes = 20
+max_lanes = 8
+max_frames = 50
+max_objects = 10
+num_agents = 4
+
+@lru_cache(128)
+def _read_csv(path: Path, *args: Any, **kwargs: Any) -> pd.DataFrame:
+    """A caching CSV reader
+
+    Args:
+        path: Path to the csv file
+        *args, **kwargs: optional arguments to be used while data loading
+
+    Returns:
+        pandas DataFrame containing the loaded csv
+    """
+    return pd.read_csv(path, *args, **kwargs)
+
+
+class exiDLoader:
+    def __init__(self, root_dir: Union[str, Path], map_path: Union[str, Path]):
+        """Initialization function for the class.
+
+        Args:
+            root_dir: Path to the folder having sequence csv files
+        """
+        self.counter: int = 0
+
+        root_dir = Path(root_dir)
+        self.seq_list: Sequence[Path] = [(root_dir / x).absolute() for x in os.listdir(root_dir)]
+        self.map_path = map_path
+        self.current_seq: Path = self.seq_list[self.counter]
+
+    @property
+    def track_id_list(self) -> List[int]:
+        """Get the track ids in the current sequence.
+
+        Returns:
+            list of track ids in the current sequence
+        """
+        _track_id_list: List[int] = np.unique(self.seq_df["trackId"].values).tolist()
+        return _track_id_list
+
+    @property
+    def num_tracks(self) -> int:
+        """Get the number of tracks in the current sequence.
+
+        Returns:
+            number of tracks in the current sequence
+        """
+        return len(self.track_id_list)
+
+    @property
+    def seq_df(self) -> pd.DataFrame:
+        """Get the dataframe for the current sequence.
+
+        Returns:
+            pandas DataFrame for the current sequence
+        """
+        return _read_csv(self.current_seq)
+
+    @property
+    def agent_lane_dict(self) -> dict():
+      agent_lane_dict = dict()
+      for id in list(self.seq_df['trackId'].unique()):
+        lanes = list(self.seq_df[self.seq_df['trackId']==id]['laneletId'].unique())
+        agent_lane_dict[id] = list()
+        for lane in lanes:
+          if len(lane) > 5:
+            agent_lane_dict[id].append(lane.split(';')[0])
+            agent_lane_dict[id].append(lane.split(';')[1])
+          else:
+            agent_lane_dict[id].append(lane)
+      return agent_lane_dict
+    
+    @property
+    def lane_centerlines_dict(self) -> dict:
+        """Get the lane-centerline id dict for the current map.
+
+        Returns:
+            dict
+        """
+        G = ox.graph_from_xml(self.map_path, simplify=False, retain_all=True)
+        gdf_nodes, gdf_edges = ox.graph_to_gdfs(G)
+
+        # 车道对应的中心线向量
+        lane_centerlines_dict = dict()  #key(lane_id), value(lane_props)
+
+        i = 0
+        id_list = []
+        for id in gdf_edges['osmid']:
+            id_list.append(id)
+            if id not in lane_centerlines_dict:
+                lane_centerlines_dict[id] = []
+            i += 1
+
+        i = 0
+        reverse_list = []
+        for r in gdf_edges['reversed']:
+            reverse_list.append(r)
+            i += 1
+
+        i = 0
+        for line in gdf_edges['geometry']:
+            line = str(line)
+            # print(str(line).split(' (')[2])
+            x0 = float(line.split('(')[1].split(' ')[0])
+            y0 = float(line.split(' ')[2].split(',')[0])
+            # long, lat -> UTM
+            xy = utm.from_latlon(latitude=y0, longitude=x0)
+            x0 = xy[0] - 352146.6
+            y0 = xy[1] - 5651141.9
+
+            id = id_list[i]
+            reverse = reverse_list[i]
+            if not reverse:
+                lane_centerlines_dict[id].append([x0, y0])
+            i += 1
+
+        # print(lane_centerlines_dict[1500])
+        return lane_centerlines_dict
+
+    @property
+    def left_right_dict(self) -> dict:
+        """Get the left-right lane id dict for the current map.
+        """
+        # 车道对应左右车道线
+        tree = ET.parse(self.map_path)
+        # 获取根节点
+        root = tree.getroot()
+        ref_df = []  #id, left, right
+
+        for r in root.iter('relation'):
+            # print(r.attrib)
+            relation = [r.attrib['id'], "", ""]
+            for mem in r[0:2]:
+            # print(mem.attrib)
+            # print(len(mem.attrib))
+                if mem.attrib['role'] == 'left':
+                    relation[1] = mem.attrib['ref']
+                elif mem.attrib['role'] == 'right':
+                    relation[2] = mem.attrib['ref']
+            ref_df.append(relation)
+
+        #建立 lanlet_id 与左右车道映射
+        left_id_dict = dict()
+        right_id_dict = dict()
+        for i in range(len(ref_df)):
+            left_id_dict[ref_df[i][0]] = ref_df[i][1]
+            right_id_dict[ref_df[i][0]] = ref_df[i][2]
+
+        return left_id_dict, right_id_dict
+
+    @property
+    def agent_traj(self) -> np.ndarray:
+        """Get the trajectory for the track of type 'AGENT' in the current sequence.
+
+        Returns:
+            numpy array of shape (seq_len x 2) for the agent trajectory
+        """
+        #test
+        # print(self.seq_df.columns.values)
+        agent_id = self.seq_df[self.seq_df["class"] == "agent"]["trackId"].unique()
+        agent_x, agent_y = [], []
+        for id in list(agent_id):
+          agent_x.append(self.seq_df[(self.seq_df["class"] == "agent") \
+                        & (self.seq_df["trackId"] == int(id))]["xCenter"])
+          agent_y.append(self.seq_df[(self.seq_df["class"] == "agent") \
+                        & (self.seq_df["trackId"] == int(id))]["yCenter"])
+        # print('agent_x: ', len(agent_x), len(agent_x[0]))
+        agent_traj = np.column_stack((agent_x, agent_y))
+        # print('agent_traj: ', agent_traj.shape)
+        return agent_traj # [4,50,50]
+
+    @property
+    def agent_info(self) -> np.ndarray:
+        """Get the trajectory for the track of type 'AGENT' in the current sequence.
+
+        Returns:
+            numpy array of shape (seq_len x 2) for the agent trajectory
+        """
+        agent_id = self.seq_df[self.seq_df["class"] == "agent"]["trackId"].unique()
+        agent_x, agent_y = [], []
+        vel_x, vel_y = [], []
+        heading = []
+        for id in list(agent_id):
+          temp_df = self.seq_df[(self.seq_df["class"] == "agent") \
+                        & (self.seq_df["trackId"] == int(id))]
+          # print('agent columns ', temp_df.columns.values)
+          agent_x.append(temp_df["xCenter"])
+          agent_y.append(temp_df["yCenter"])
+          vel_x.append(temp_df["xVelocity"])
+          vel_y.append(temp_df["yVelocity"])
+          heading.append(temp_df["heading"])
+
+        agent_info = np.column_stack((agent_x, agent_y, vel_x, vel_y, heading))
+        # print('agent_info: ', agent_info.shape, agent_info[0,:])
+        return agent_info # [4,50,5] agents, frames, features
+
+    @property
+    def obj_info(self) -> np.ndarray:
+        """Get the trajectory for the track of type 'OTHER' in the current sequence.
+
+        Returns:
+            numpy array of shape (seq_len x 2) for the agent trajectory
+        """
+        obj_id = self.seq_df[self.seq_df["class"] != "agent"]["trackId"].unique()
+        agent_x, agent_y = [], []
+        vel_x, vel_y = [], []
+        heading = []
+        start_frame = min(self.seq_df["frame"])
+        end_frame = max(self.seq_df["frame"])
+        # print('start - end ', start_frame, end_frame)
+        for id in list(obj_id):
+          temp_df = self.seq_df[(self.seq_df["class"] != "agent") \
+                        & (self.seq_df["trackId"] == int(id))].reset_index()
+          # print(temp_df["xCenter"][0:5])
+          # 序列缺失值填补
+          df_start = max(0, min(temp_df["frame"]) - start_frame)
+          df_end = min(max(temp_df["frame"]) - start_frame + 1, max_frames)
+          # print('df ', df_start, df_end, len(temp_df["xCenter"]))
+          temp_x, temp_y, temp_vx, temp_vy, temp_h = [0]*max_frames,[0]*max_frames,[0]*max_frames,[0]*max_frames,[0]*max_frames
+          # print( temp_df["xCenter"][0])
+          for i in range(df_start, df_end):
+            temp_x[i] = temp_df["xCenter"][i-df_start]
+            temp_y[i] = temp_df["yCenter"][i-df_start]
+            temp_vx[i] = temp_df["xVelocity"][i-df_start]
+            temp_vy[i] = temp_df["yVelocity"][i-df_start]
+            temp_h[i] = temp_df["heading"][i-df_start]
+          # print('temp x ', temp_x)
+          agent_x.append(temp_x)
+          agent_y.append(temp_y)
+          vel_x.append(temp_vx)
+          vel_y.append(temp_vy)
+          heading.append(temp_h)
+
+        obj_info = np.column_stack((agent_x, agent_y, vel_x, vel_y, heading))
+        # print('obj_info ', obj_info.shape)
+        return obj_info # [4,50,5] objs, frames, features
+
+
+    def __iter__(self) -> "exiDLoader":
+        """Iterator for enumerating over sequences in the root_dir specified.
+
+        Returns:
+            Data Loader object for the first sequence in the data
+        """
+        self.counter = 0
+        return self
+
+    def __next__(self) -> "exiDLoader":
+        """Get the Data Loader object for the next sequence in the data.
+
+        Returns:
+            Data Loader object for the next sequence in the data
+        """
+        if self.counter >= len(self):
+            raise StopIteration
+        else:
+            self.current_seq = self.seq_list[self.counter]
+            self.counter += 1
+            return self
+
+    def __len__(self) -> int:
+        """Get the number of sequences in the data
+
+        Returns:
+            Number of sequences in the data
+        """
+        return len(self.seq_list)
+
+    def __str__(self) -> str:
+        """Decorator that returns a string storing some stats of the current sequence
+
+        Returns:
+            A string storing some stats of the current sequence
+        """
+        return f"""Seq : {self.current_seq}
+        ----------------------
+        || City: {self.city}
+        || # Tracks: {len(self.track_id_list)}
+        ----------------------"""
+
+    def __getitem__(self, key: int) -> "exiDLoader":
+        """Get the DataLoader object for the sequence corresponding to the given index.
+
+        Args:
+            key: index of the element
+
+        Returns:
+            Data Loader object for the given index
+        """
+
+        self.counter = key
+        self.current_seq = self.seq_list[self.counter]
+        return self
+
+    def get(self, seq_id: Union[Path, str]) -> "exiDLoader":
+        """Get the DataLoader object for the given sequence path.
+
+        Args:
+            seq_id: Fully qualified path to the sequence
+
+        Returns:
+            Data Loader object for the given sequence path
+        """
+        self.current_seq = Path(seq_id).absolute()
+        return self
+
